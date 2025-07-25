f489a0965f6b1a83d4c3472197f446be70e52bd6
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-07-25 11:22:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+// number
+// bigInt
+// string
+// boolean
+// null  => standalone value
+// symbol => unique
+
+// object
+
+let name = "Anuj"
+let age = 22
+let isLoggedIn = false
+
+console.log(typeof "anuj");
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
