1092850a5e3b74abaeda7887a61ff1419b6a813e
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-07-25 12:02:29 +0000

    learned abount conversion and operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,57 @@
+let score = "55"
+
+// console.log(typeof score); // string
+// console.log(typeof (score)); // string
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);// number
+// console.log(valueInNumber); // 55
+
+let table = "33abc"
+let tableInNumber = Number(table)
+// console.log(typeof tableInNumber); // number
+// console.log(tableInNumber); // NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+let isLoggedIn = 1
+let bool = Boolean(isLoggedIn)
+// console.log(typeof bool);
+// console.log(bool);
+// console.log(typeof isLoggedIn);
+
+
+// 0 => false , 1 => true
+// "" => false 
+// "anuj" => true
+
+
+// *********************** Operations ***********************
+
+
+let value = 6
+let negValue = -value
+// console.log(negValue);
+
+let str1  = "hello"
+let str2 = " anuj"
+// console.log(str1+str2);
+
+// console.log(1 + "2");  // 12
+// console.log(1 + 1 + "2"); // 22
+// console.log("1" + 1 + 2); // 112
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
