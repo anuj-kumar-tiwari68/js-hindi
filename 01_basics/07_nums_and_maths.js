646cee96385aaf8f7ff22af75a3b5408190c8cc6
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-07-27 15:25:38 +0000

    learned about numbers in js

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 100
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));// used mostly 
+
+const num2 = 45.68765
+// console.log(num2.toPrecision(3));// 45.7 
+// console.log(num2.toPrecision(2));// 46
+
+const num3 = 1000000
+console.log(num3.toLocaleString()); // 1,000,000
+console.log(num3.toLocaleString('en-IN')); // 10,00,000
+
+
+
+
+
+
