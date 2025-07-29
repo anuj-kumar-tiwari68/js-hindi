58a2e60857874c4c34da1b87a2a2f24290255d26
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-07-29 17:58:51 +0000

    Array part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,47 @@
+// arrays
+
+const myArray = [0,1,2,3,4,5]
+// console.log(myArray);
+
+
+const newArray = new Array(1,2,3,4,5)
+// console.log(newArray);
+
+// Array Methods
+
+// myArray.push(6)
+// console.log(myArray);
+
+// newArray.pop()
+// console.log(newArray);
+
+// myArray.unshift(-1)
+// console.log(myArray);
+
+// myArray.shift()
+// console.log(myArray);
+
+// console.log(myArray.includes(10));
+// console.log(myArray.indexOf(4));
+
+// const newArr = myArray.join()
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+ 
+// slice and splice
+
+console.log('A ',myArray);
+const myna1 = myArray.slice(1,3)
+console.log(myna1);
+
+console.log('B ',myArray);
+
+const myna2 = myArray.splice(1,3)
+console.log(myna2);
+console.log('c ',myArray);
+
+
+
+
