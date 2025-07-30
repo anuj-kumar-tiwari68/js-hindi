08d85cf8340d57e1ee021f0144234fd552e25c5a
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-07-30 18:01:33 +0000

    Objects part 2

---
+++ 02_basics/04_Obejects.js
@@ -0,0 +1,56 @@
+// const tinderUser = new Object()// way 1  // singleton Object
+const tinderUser = {}  // way 2  //  non-singleton Object
+
+tinderUser.id = "123abc"
+tinderUser.name = "sam"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+const somename ={}
+somename.email = "abcs@chatgpt.com"
+somename.fulName = {
+    somefullname : {
+        first : "lalit",
+        middle : "kumar",
+        last : "Sharme"
+    }
+}
+
+// console.log(somename);
+
+const obj1 = {1:'a',2:'b',3:'c'}
+const obj2 = {4:'d',5:'e',6:'f'}
+
+// const combined = Object.assign(obj2,obj1) // way 1
+// const combined = Object.assign({},obj2,obj1) // way 2
+
+const combined = {...obj1,...obj2}// correct method
+// console.log(combined);
+// console.log(obj1);
+
+const users = [
+    {
+        id:1,
+        name:'abc'
+    },
+     {
+        id:1,
+        name:'abc'
+    },
+     {
+        id:1,
+        name:'abc'
+    }
+]
+
+users[1].name
+// console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name'));
+
