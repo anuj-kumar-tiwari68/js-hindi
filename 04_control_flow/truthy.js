60c96e921eafb87837344b370a9771c2a7fe8ca5
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-08-02 04:04:05 +0000

    switch and truthy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,12 @@
+const userEmail = "a@anuj.ai"
+if (userEmail) {
+    console.log("got the user email");
+    
+}else {
+    console.log("no user email");
+    
+}
+
+// falsy values => false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values => "0", "false", " ", [], {}, function(){}
