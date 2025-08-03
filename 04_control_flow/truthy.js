d3a0315d36bebf5c2795be7fa59e50e89ef247fa
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-08-03 15:28:41 +0000

    ternary operator and nullish operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,12 +1,38 @@
-const userEmail = "a@anuj.ai"
-if (userEmail) {
-    console.log("got the user email");
+// const userEmail = "a@anuj.ai"
+// if (userEmail) {
+//     console.log("got the user email");
     
-}else {
-    console.log("no user email");
+// }else {
+//     console.log("no user email");
     
-}
+// }
 
 // falsy values => false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
 // truthy values => "0", "false", " ", [], {}, function(){}
+
+// const emptyObj = {}
+
+// // if (Object.keys(emptyObj).length === 0) {
+// //     console.log("Object is empty ");
+    
+// // }
+
+// // Nullish Coalescing Operator (??) : null undefined
+
+// let val1;
+// // val1 = 5 ?? 10 // 5
+// // val1 = null ?? 10 // 10
+// val1 = undefined ?? 15 // 15
+// val1 = null ?? 10 ?? 15 // 15
+// console.log(val1);
+
+
+// ternary operator
+
+// condition ? true : false
+
+const icsTeaPrice = 100
+icsTeaPrice <= 80 ? console.log("You can buy it"): console.log("I don't have that much money");
+
+
