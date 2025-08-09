12d9be8727c3f67592095c1746848b551ee933e7
anuj-kumar-tiwari68 <anujkumartiwari68@gmail.com>
2025-08-09 18:08:23 +0000

    new pattern

--- 05_iterations/09_pattern.js
+++ 05_iterations/09_pattern.js
@@ -1,10 +1,21 @@
-for (let i = 1; i <= 5; i++) {
-  let row = "";
-  let spaces;
+// for (let i = 1; i <= 5; i++) {
+//   let row = "";
+//   let spaces;
 
-  for (let j = 1; j <= i; j++) {
-    spaces = " ".repeat(5 - i);
-    row += "#";
+//   for (let j = 1; j <= i; j++) {
+//     spaces = " ".repeat(5 - i);
+//     row += "#";
+//   }
+//   console.log(spaces + row);
+// }
+for (let i=0;i<5;i++){
+  let row = ""
+  let spaces = ""
+  for(let j=i-1;j<=i;j--){
+    spaces += " "
   }
-  console.log(spaces + row);
+  for(let k=1;k<=i;k++){
+    row += "#"
+  }
+  console.log(spaces + row)
 }
